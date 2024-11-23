faebaf6752c596c1045eb143e55f7a2c0d31a5f0
Pravin N <pravin445518@gmail.com>
2024-11-23 14:12:59 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # GitHub API URL
+helper()
+
 API_URL="https://api.github.com"
 
 # GitHub username and personal access token
@@ -34,6 +36,13 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+function helper{
+expected_cmd_args=2
+if[ $# -ne $expected_cmd_args];then
+echo "please execute the script with required cmd args "
+echo "asd"
+fi
 }
 
 # Main script
